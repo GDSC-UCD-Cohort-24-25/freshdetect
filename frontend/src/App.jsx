4f2afce80c0d0207f171d90d2869510107feabb7
Yoshiki Nakajima <ynaka@ucdavis.edu>
2025-03-06 08:09:14 +0000

    Modified App.jsx Output correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect }from 'react';
-import ImgIn from './components/ImgIn';
+import ImgIn from './components/ImgIn.jsx';
+import Result from './components/Result.jsx';
+// import ".styles/App.css"
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 function App() {
 
     return (
-    <>
-        <div className = "AppContainer">
+        <>
+        {/* <h1>Hello</h1>  */}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element = {<ImgIn />} />
+                <Route path="/result" element = {<Result />} />
+            </Routes>
+        </BrowserRouter>
+
+        {/* <div className = "AppContainer">
         <ImgIn/>
-        </div>
-        
-    </>
+        </div> */}
+        </>
     );
 }
 
