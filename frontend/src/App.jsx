0781050769410f54e290b4ea662ec07a354b1a54
Yoshiki Nakajima <ynaka@ucdavis.edu>
2025-03-08 11:26:27 +0000

    Modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,33 @@
 import React, { useState, useEffect }from 'react';
 import ImgIn from './components/ImgIn.jsx';
 import Result from './components/Result.jsx';
-// import ".styles/App.css"
+import "./styles/App.css";
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { GiChickenOven } from "react-icons/gi";
+
+
+function Header(){
+    return (
+        <header className="header">
+            <GiChickenOven className="icon" />
+            <h1 style={{marginLeft: "12px" ,marginTop: "10px" ,fontFamily:"Pacifico" }}>meetchecker</h1>
+        </header>
+    );
+}
+
 
 function App() {
 
     return (
         <>
-        {/* <h1>Hello</h1>  */}
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element = {<ImgIn />} />
                 <Route path="/result" element = {<Result />} />
             </Routes>
         </BrowserRouter>
-
-        {/* <div className = "AppContainer">
-        <ImgIn/>
-        </div> */}
+        
         </>
     );
 }
